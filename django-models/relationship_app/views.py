fb240ef8ddf48a429a55727b4712686fe57bba40
Victor Omolo <niphia2060@gmail.com>
2024-08-17 13:33:16 +0300

    Add user authentication views and templates

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 def list_books(request):
     books = Book.objects.all()
@@ -16,3 +20,15 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.objects.books.all()
         return context
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page or another page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
