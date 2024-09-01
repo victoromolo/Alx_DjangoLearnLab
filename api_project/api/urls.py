48e040edff25ff4855baa233acc3f050753412c4
Victor Omolo <niphia2060@gmail.com>
2024-09-01 16:21:57 +0300

    implemented authentication and permissions in django rest framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from .views import BookList, BookViewSet
 from . import views
 from rest_framework.routers import DefaultRouter
-
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -11,4 +10,5 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -12,4 +13,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
     
