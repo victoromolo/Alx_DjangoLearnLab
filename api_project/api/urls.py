826084aa80593c7ad974122f2b871870c4f861ef
Victor Omolo <niphia2060@gmail.com>
2024-09-01 15:27:11 +0300

    implimented crud operations with viewsets

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
